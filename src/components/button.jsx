491e9fbdf43d9802006b1bcb1839b931d6d6bed3
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-04-08 20:43:08 +0200

    boton

---
+++ src/components/button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Button() {
+  return (
+    <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg">
+      Botón Verde
+    </button>
+  )
+}
+
+export default Button;
