63bc489453e708e6a8e340dfeda0cc5b31656f3a
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-04-09 20:24:20 +0200

    Añadir boton de compras y utilizar el componente button

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 
-function Button() {
+function Button({ onClick, texto, className }) {
   return (
-    <>
-      <div className="container py-10 px-10 mx-0 min-w-full flex flex-col gap-4 items-center">
-        <button className="mi-boton bg-blue-500 hover:bg-blue-600">
-          Azul
-        </button>
-        <button className="mi-boton bg-green-500 hover:bg-green-600">
-          Verde
-        </button>
-      </div>
-    </>
-  )
+    <button className={className} onClick={onClick}>
+      {texto}
+    </button>
+  );
 }
 
 export default Button;
--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from './button';
 import { useState } from 'react';
 
 function ProductCard({ product }) {
@@ -7,6 +8,10 @@ function ProductCard({ product }) {
   const handleLike = () => {
     setLikes(likes + 1);
   };
+
+  const handleAddToCart = (producto) => {
+    console.log(`Producto ${producto} añadido al carrito`);
+  }
 
   return (
     <div className="bg-white shadow-md rounded-lg overflow-hidden max-w-sm mx-auto w-full sm:w-80 md:w-96 lg:w-[500px]">
@@ -19,12 +24,17 @@ function ProductCard({ product }) {
         <h3 className="text-lg font-bold text-gray-800">{product.nombre}</h3>
         <p className="text-gray-600 mt-2">${product.precio.toFixed(2)}</p>
         <div className="flex justify-between items-center mt-4">
-          <button
+          <Button
             onClick={handleLike}
+            texto={`Me gusta ${likes}`}
             className="mi-boton bg-indigo-400 hover:bg-indigo-700 transition"
-          >
-            Me gusta ({likes})
-          </button>
+          />
+
+          <Button
+            onClick={() => handleAddToCart(product.nombre)}
+            texto={`Añadir al carrito`}
+            className="mi-boton bg-indigo-400 hover:bg-indigo-700 transition"
+          />
         </div>
       </div>
     </div>
