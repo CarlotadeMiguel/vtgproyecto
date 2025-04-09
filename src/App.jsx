e9f8181570d4616021b6dc434a325b5e300a3270
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-04-09 17:19:22 +0200

    añadir cabecera con navegacion

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,17 @@ import Grid from './components/grid';
 function App() {
   return (
     <>
+      <div>
+        <header>
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </header>
+      </div>
       <Button />
       <Grid />
     </>
